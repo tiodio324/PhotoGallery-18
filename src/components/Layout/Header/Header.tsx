ac494ba1c47a9883bb8178898fc51ab435c9af56
varya2525varya-beep <varya2525varya@gmail.com>
2026-06-18 09:49:20 +0300

    Update Header.tsx

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -9,7 +9,7 @@ export const Header = observer(() => {
 
   const getRoleName = (role: string): string => {
     switch (role) {
-      case 'admin': return 'Администратор';
+      case 'admin': return 'Фотограф';
       case 'photographer': return 'Фотограф';
       default: return 'Гость';
     }
