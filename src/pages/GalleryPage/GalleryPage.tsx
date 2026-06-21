7cc262696c1bd321f8072e96641d2229bb62effb
varya2525varya-beep <varya2525varya@gmail.com>
2026-06-21 13:19:01 +0300

    Update GalleryPage.tsx

--- src/pages/GalleryPage/GalleryPage.tsx
+++ src/pages/GalleryPage/GalleryPage.tsx
@@ -134,7 +134,7 @@ export const GalleryPage = observer(() => {
               <polyline points="7 10 12 15 17 10" />
               <line x1="12" y1="15" x2="12" y2="3" />
             </svg>
-            Скачать оригинал
+            Скачать
           </a>
      )}
               {selectedPhoto.copyright && !String(selectedPhoto.copyright).startsWith('data:image') && (
